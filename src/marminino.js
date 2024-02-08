939955746c27114732cca806446bc3417f3e64e8
Lucivando Sosua <lucivandosousa@gmail.com>
2024-02-08 15:40:58 -0300

    subir desafio

--- src/marminino.js
+++ src/marminino.js
@@ -1,8 +1,22 @@
 function executar(numero) {
+    if (numero % 3 == 0) {
+        return "Mar";
+    }
+
+    if (numero % 4 == 0) {
+        return "Minino";
+    }
+
+    if (numero % 3 == 0 && numero % 4 == 0) {
+        return "MarMinino";
+    }
+
     return "";
-    //return "Mar";
-    //return "Minino"
-    //return "MarMinino";
+}
+
+for (let index = 1; index <= 100; index++){
+    const resultado = executar(index)
+    //console.log(resultado)
 }
 
 module.exports = executar;
